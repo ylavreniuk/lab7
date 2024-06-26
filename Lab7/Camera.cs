34561985258ddc0a7ae0948f7afc827170cee393
ylavreniuk <87333253+ylavreniuk@users.noreply.github.com>
2024-06-26 19:12:23 +0300

    Add files via upload

---
+++ Lab7/Camera.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+
+namespace ClassLib
+{
+    public class Camera : IComparable
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Contry { get; set; }
+        public int SceenDiagonal { get; set; }
+        public int MatrixSize { get; set; }
+        public double YearOfProduction { get; set; }
+        public double Weight { get; set; }
+        public bool LensInterchangeable { get; set; }
+
+        public double Price
+        {
+            get { return MatrixSize * YearOfProduction; }
+        }
+        public Camera()
+        {
+        }
+
+        public Camera(string name, string model, string contry, int sceenDiagonal,
+            int matrixSize, double yearOfProduction,
+            double weight, bool lensInterchangeable)
+        {
+            Name = name;
+            Model = model;
+            Contry = contry;
+            SceenDiagonal = sceenDiagonal;
+            MatrixSize = matrixSize;
+            YearOfProduction = yearOfProduction;
+            Weight = weight;
+            LensInterchangeable = lensInterchangeable;
+            
+
+        }
+        public string Info()
+        {
+            return Name + ", " + Model + ", " +Price;
+        }
+        public int CompareTo(object obj)
+        {
+            Camera b = obj as Camera;
+            return string.Compare(this.Name, b.Name);
+        }
+    }
+}
+
+
+
---
+++ Lab7/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLib;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static List<Camera> Cameras; 
+
+        static void PrintCameras() 
+        {
+            foreach (var camera in Cameras) 
+            {
+                Console.WriteLine(camera.Info());
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Cameras = new List<Camera>(); 
+            FileStream fs = new FileStream("Camera.camera", FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+            try
+            {
+                Console.WriteLine("Читаємо дані з файлу...\n");
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    Camera camera1 = new Camera();
+                    for (int i = 0; i < 9; i++)
+                    {
+                        switch (i)
+                        {
+                            case 1:
+                                camera1.Name = br.ReadString();
+                                break;
+                            case 2:
+                                camera1.Model = br.ReadString();
+                                break;
+                            case 3:
+                                camera1.Contry = br.ReadString();
+                                break;
+                            case 4:
+                                camera1.SceenDiagonal = br.ReadInt32();
+                                break;
+                            case 5:
+                                camera1.MatrixSize = br.ReadInt32();
+                                break;
+                            case 6:
+                                camera1.YearOfProduction = br.ReadDouble();
+                                break;
+                            case 7:
+                                camera1.Weight = br.ReadDouble();
+                                break;
+                            case 8:
+                                camera1.LensInterchangeable = br.ReadBoolean();
+                                break;
+                        }
+                    }
+                    Cameras.Add(camera1); 
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Сталась помилка: {0}", ex.Message);
+            }
+            finally
+            {
+                br.Close();
+            }
+
+            Console.WriteLine($"Несортований перелік камер: {Cameras.Count}");
+            PrintCameras(); 
+            Cameras.Sort();
+            Console.WriteLine($"Сортований перелік камер: {Cameras.Count}");
+            PrintCameras();
+
+            Camera camera = new Camera("Nikon", "D3500", "Japan", 3, 24, 2019, 365, true);
+            Cameras.Add(camera); 
+            Cameras.Sort();
+            Console.WriteLine($"Перелік камер: {Cameras.Count}");
+            PrintCameras();
+
+            Console.WriteLine("Видаляємо останнє значення");
+            Cameras.RemoveAt(Cameras.Count - 1); 
+            Console.WriteLine($"Перелік камер: {Cameras.Count}");
+            PrintCameras(); 
+
+            Console.ReadKey();
+        }
+    }
+}
